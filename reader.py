9dd6eefcac310b159f4b6b784a0dc16c4e26b55e
Oleg S home <tipkor@mail.ru>
2024-02-18 18:45:14 +0300

    по типу контента перебор в сообщениях.

--- reader.py
+++ reader.py
@@ -14,30 +14,30 @@ HOST = "imap.spaceweb.ru"
 
 
 def write_to_f(data):
-    with open('1.txt', 'w', encoding='utf-8') as f:
+    with open('1.txt', 'a', encoding='utf-8') as f:
         f.write(data + '\n')
 
 
 def print_msg(data: List[Message, ]):
-    """берем список мейл сообщений, по каждому проходим вглубь, определяем тип контента.
-    на основе типа контента, надо декодировать инфу.
-    Нужно:
-    - от кого, кому, тема
-    - тело сообщения
-    - хтмл если есть
-    - аттач, если есть. (попробовать побайтово)
-    
-    """
-    msg_info = {}
-    count = 0
-    for i in data:
-        count += 1
-        for part in i.walk():
-            if i.get_content_type():
-                write_to_f(i.get_content_type())
-    # write_to_f(pprint.pformat(msg_info))
-    
+    """берем список сообщений, и в каждом проходим во вложенные.
+    определяем тип и забираем.
+    типы: текст, штмл, аттач.
+    - вложенность, как определить?
 
+    """
+
+    # https://habr.com/ru/articles/17531/
+    for i in data:
+        for part in i.walk():
+            if part.get_content_type():
+                content_type = part.get_content_type()
+                write_to_f('--------------' + content_type + '--------------')
+
+                if content_type == 'text/plain':
+                    write_to_f(part.get_payload())
+                elif content_type == 'text/html':
+                    write_to_f(part.get_payload())
+            break
 
 # Подключение к серверу IMAP
 mail = imaplib.IMAP4_SSL(HOST)
@@ -48,11 +48,12 @@ mail.select('Inbox')
 status, num = mail.search(None, 'ALL')
 all_messages = []
 # перебираем сообщения по номерам
-for _ in num[0].split():
+nums = num[0].split()
+for _ in nums[:3]:
     status, data = mail.fetch(_, '(RFC822)')
     msg_body = data[0][1].decode('utf-8')
     email_message = email.message_from_string(msg_body)
     all_messages.append(email_message)
-    break
+
 
 print_msg(all_messages)
