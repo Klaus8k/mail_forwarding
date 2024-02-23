fb2b7a7ab05aa1b7d43656811decfa4a70dd8b66
Klaus8k <klaus888@yandex.ru>
2024-02-23 14:58:26 +0300

    file send

--- reader.py
+++ reader.py
@@ -1,3 +1,5 @@
+from email.policy import default
+from email.parser import BytesParser, Parser
 import base64
 import pprint
 import json
@@ -5,53 +7,17 @@ import imaplib
 import email
 from email.message import Message
 from typing import List
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+import quopri
+import smtplib
+
 # https://stackoverflow.com/questions/1463074/how-can-i-get-an-email-messages-text-content-using-python
 
 const_dict = json.load(open("const.json", "r"))
 SOURCE_ADDR = const_dict["email"]
 PSWD = const_dict["pass"]
 HOST = "imap.spaceweb.ru"
-
-
-def write_to_f(data):
-    with open('1.txt', 'a', encoding='utf-8') as f:
-        f.write(data + '\n')
-
-
-def print_msg(data: List[Message, ]):
-    """берем список сообщений, и в каждом проходим во вложенные.
-    определяем тип и забираем.
-    типы: текст, штмл, аттач.
-    - вложенность, как определить?
-    """
-
-    # https://habr.com/ru/articles/17531/
-    for i in data:
-        write_to_f(i.get('Return-path'))
-        write_to_f(i.get('Date'))
-        # write_to_f(i.get('Subject'))
-
-        for part in i.walk():
-            if part.get_content_type():
-                content_type = part.get_content_type()
-                write_to_f('content_type_content_type ---' +
-                           content_type + '--- content_type_content_type')
-
-                if content_type == 'text/plain' or content_type == 'text/html':
-                    if part._headers[1][1] == 'base64':
-                        write_to_f('base64 ' * 10)
-                        content = part.get_payload(
-                            decode=True).decode('utf-8')
-                        # html обработывать
-                        write_to_f(pprint.pformat(content))
-                        write_to_f('base64 ' * 10)
-                    else:
-                        write_to_f(part.get_payload())
-                        try:
-                            write_to_f(part.get_payload(decode=True))
-                        except:
-                            pass
-        write_to_f(' END ' * 10)
 
 
 # Подключение к серверу IMAP
@@ -62,19 +28,107 @@ mail.select('Inbox')
 # Получение сообщений
 status, num = mail.search(None, 'ALL')
 all_messages = []
+
+
+def write_to_f(data):
+    with open('1.txt', 'a', encoding='utf-8') as f:
+        f.write(data + '\n')
+
+
+def bs64_decode(data: str):
+    return base64.b64decode(data).decode('utf-8')
+
+
+def sender_msg(message: dict):
+    smtp = smtplib.SMTP(host=HOST, port=587)
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+    smtp.login(SOURCE_ADDR, PSWD)
+    msg = MIMEMultipart()
+    msg['From'] = SOURCE_ADDR
+    msg['To'] = 'tipkor@mail.ru'
+    
+    f_name = '1.pdf'
+    msg.attach(MIMEApplication(message['file'], Name=f_name))
+    msg['Content-Disposition'] = 'attachment; filename="%s"' % f_name
+    smtp.send_message(msg=msg, from_addr=SOURCE_ADDR, to_addrs=msg['To'])
+    
+    smtp.close()
+
+
+def print_msg(data: List[Message, ]):
+
+
+    for msg in data:
+
+        msg_headers = Parser(policy=default).parsestr(
+            msg.as_string()
+        )
+
+        message = {}
+
+        for part in msg.walk():
+            msg_headers = Parser(policy=default).parsestr(
+                part.as_string()
+            )
+
+            if not part.is_multipart():
+
+                content_type = msg_headers['Content-Type'].split()[0][:-1]
+                content_encoding = msg_headers['Content-Transfer-Encoding']
+
+                # is_quoted-printable
+                if content_encoding == 'quoted-printable':
+                    if part.get_charset() == 'utf-8':
+                        write_to_f('\nis_quoted-printable')
+                        write_to_f(quopri.decodestring(
+                            part.get_payload()).decode('utf-8'))
+                    if part.get_charset() == 'windows-1251':
+                        write_to_f('\nis_quoted-printable')
+                        write_to_f(quopri.decodestring(
+                            part.get_payload()).decode('windows-1251'))
+
+                if content_type == 'text/plain':
+                    write_to_f(content_type)
+                    write_to_f(content_encoding)
+                    if content_encoding == 'base64':
+                        write_to_f('\nis_base64')
+                        write_to_f(bs64_decode(part.get_payload()))
+                    else:
+                        write_to_f(part.get_payload())
+
+                elif content_type == 'text/html':
+                    write_to_f(content_type)
+                    write_to_f(content_encoding)
+                    if content_encoding == 'base64':
+                        write_to_f('\nis_base64')
+                        write_to_f(bs64_decode(part.get_payload()))
+                    else:
+                        write_to_f(part.get_payload())
+
+                elif part.get_content_disposition() == 'attachment': # Разобраться как сохранить файл.
+                    message['file'] = part.as_bytes()
+                    print(message['file'])
+                    write_to_f(f'attachment -----------> {content_type}')
+
+                else:
+                    write_to_f(
+                        f'!!!!!!!!!!!!!!! content type Который не обработан: {content_type}')
+
+        write_to_f(' END ' * 10)
+        sender_msg(message=message)
+
+
 # перебираем сообщения по номерам
 nums = num[0].split()
-for _ in nums[20:]:
-    try:
-        write_to_f(str(_) * 50)
-        status, data = mail.fetch(_, '(RFC822)')
+for _ in nums[:1]:
 
-        msg_body = data[0][1].decode('utf-8')
+    status, data = mail.fetch(_, '(RFC822)')
 
-        email_message = email.message_from_string(msg_body)
-        all_messages.append(email_message)
-    except:
-        pass
+    msg_body = data[0][1].decode('utf-8', errors='replace')
 
-print(all_messages)
+    email_message = email.message_from_string(msg_body)
+    all_messages.append(email_message)
+
 print_msg(all_messages)
